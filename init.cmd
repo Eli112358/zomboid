cd67443e9cdc0ee64bfbbd95592c615fe5800cd3
Eli112358 <Eli112358D@gmail.com>
2022-03-06 13:31:40 -0500

    Improves init script

--- init.cmd
+++ init.cmd
@@ -1,5 +1,6 @@
 @echo off
 path %~dp0src;%path%
-pushd %userprofile%\Zomboid
-%~dp0venv\Scripts\activate 2>nul
+pushd %userprofile%\Zomboid\Saves
+prompt ~\Zomboid$+$g
 cls
+%~dp0venv\Scripts\activate 2>nul
